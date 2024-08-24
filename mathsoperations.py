17ab1550474ebbd854509ea6eecbacd97f00a886
Chandrasekhar8886 <chandrasekharreddy8886@gmail.com>
2024-08-24 12:42:47 +0530

    adding my file

---
+++ mathsoperations.py
@@ -0,0 +1,12 @@
+z1 = complex(4, 3)  # 4 + 3i
+z2 = complex(2, -1)  # 2 - i
+addition = z1 + z2
+print(f"Addition of {z1} and {z2} is {addition}")
+subtraction = z1 - z2
+print(f"Subtraction of {z1} and {z2} is {subtraction}")
+multiplication = z1 * z2
+print(f"Multiplication of {z1} and {z2} is {multiplication}")
+conjugate_z1 = z1.conjugate()
+conjugate_z2 = z2.conjugate()
+print(f"Conjugate of {z1} is {conjugate_z1}")
+print(f"Conjugate of {z2} is {conjugate_z2}")
